783bb61baa56e19d3ca245cf9c6de31097100721
Lukas Huber <lukashuber1@gmail.com>
2022-11-11 16:25:05 +0100

    feat: update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Example Intelligent Adaptive Systems
+
+In this Repository, I collect Orange3 examples and python scripts for Intelligent Adaptive Systems.
+
+The python code can be executed in cells like in Jupyter Notebooks. The Orange3 examples can be executed in Orange3.
+
+## Orange3 Examples
--- classification.py
+++ classification.py
@@ -20,19 +20,19 @@ df.plot(
 # %% Logistic Regression
 from sklearn.linear_model import  LogisticRegression
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
 
 log_regressor = LogisticRegression(solver="lbfgs")  
 log_regressor.fit(df[['x1', 'x2']], df[['c']].to_numpy().ravel())
 log_prediciton = log_regressor.predict(df[['x1','x2']])
 
 
-confusion_matrix(  
+cm = confusion_matrix(  
     y_true=df[['c']].to_numpy().ravel(),  
     y_pred=log_prediciton
 )
-plot_confusion_matrix(log_regressor, df[['x1', 'x2']], df[['c']].to_numpy().ravel())  
-
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["0", "1"])  
+disp.plot()
 # %%
 from sklearn.tree import DecisionTreeClassifier
 
@@ -40,11 +40,12 @@ tree_classifyer = DecisionTreeClassifier()
 tree_classifyer.fit(df[['x1', 'x2']], df[['c']].to_numpy().ravel())
 tree_prediction = tree_classifyer.predict(df[['x1', 'x2']])
 
-confusion_matrix(
+tree_cm = confusion_matrix(
     y_true=df[['c']].to_numpy().ravel(),  
     y_pred=tree_prediction
 )
-plot_confusion_matrix(tree_classifyer, df[['x1', 'x2']], df[['c']].to_numpy().ravel())  
+disp = ConfusionMatrixDisplay(confusion_matrix=tree_cm, display_labels=["0", "1"])
+disp.plot()  
 # %%
 from sklearn import tree
 tree.plot_tree(tree_classifyer)
